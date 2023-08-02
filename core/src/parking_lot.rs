caaf514062abcaec19f0de17981725f92bc5f3e4
nneesshh <nneesshh@163.com>
2023-08-02 18:24:39 +0800

    make warning silence

--- core/src/parking_lot.rs
+++ core/src/parking_lot.rs
@@ -4,7 +4,8 @@
 // http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
 // http://opensource.org/licenses/MIT>, at your option. This file may not be
 // copied, modified, or distributed except according to those terms.
-use crate::thread_parker::{ThreadParker, ThreadParkerT, UnparkHandleT};
+//use crate::thread_parker::{ThreadParker, ThreadParkerT, UnparkHandleT};
+use crate::thread_parker::ThreadParker;
 use crate::util::UncheckedOptionExt;
 use crate::word_lock::WordLock;
 use core::{
